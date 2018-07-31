910046b58a57228c64b8282fd7ba6a51ada49872
CNadjim <c.nadjim@gmail.com>
2018-07-31 10:58:05 +0200

    Automatic deploy on prod env

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull nadjim/bettor-league-api
-docker-compose -f /$DEPLOY_PATH/docker-compose.yml down
-docker-compose -f /$DEPLOY_PATH/docker-compose.yml up -d
+docker-compose -f /spring-boot-server/docker-compose.yml down
+docker-compose -f /spring-boot-server/docker-compose.yml up -d
